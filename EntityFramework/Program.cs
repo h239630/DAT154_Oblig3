7d08a2847451513bd9b58bf8a4810e5b0d228103
Mikael Lindebrekke <239630@stud.hvl.no>
2022-03-27 12:45:58 +0200

    Add project files.

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dat154Entities dbContext = new dat154Entities();
+
+            DbSet<student> students = dbContext.student;
+
+            foreach (student student in students.Where(student => student.studentname.Contains("k")))
+            {
+                Console.WriteLine(student.studentname); 
+            }
+            Console.WriteLine("\n\nPress a key to end...");
+            Console.ReadLine();
+        }
+    }
+}
